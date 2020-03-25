64bd65df730cbc5b877039828a2fc656af1aa4a1
Zoe Stokes <sekotszs@gmail.com>
2020-03-25 14:30:17 -0700

    more work

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
+// setting up validations
 const formSchema = yup.object().shape({
     name:yup.string().required("Name is required"),
     email: yup.string().email().required("Email is required"),
@@ -9,6 +10,7 @@ const formSchema = yup.object().shape({
     terms: yup.boolean().oneOf([true], "Agree to the terms or else")
 })
 
+//setting up the form states
 function Form() {
     const [formState, setFormState] = useState({
         name: '',
@@ -17,13 +19,39 @@ function Form() {
         terms: '',
     })
 
+//setting up state for errors
     const[errors, setErrors] = useState({
         name:'',
         email:'',
         password:'',
         terms:'',
     });
+    
+//setting state for button as disbaled
     const [buttonDisabled, setButtonDisabled] = useState(true);
+
+// validating use effect
+    useEffect(() =>{
+    formSchema.isValid(formState).then(valid =>{
+        setButtonDisabled(!valid);
+    });
+    }, [formState]);
+
+//validating change
+    const validateChange = e => {
+        yup.reach(formSchema, e.target.name).validate(e.target.value).then(valid => 
+        {setErrors({
+        ...errors,
+        [e.target.name]:''
+        })
+    })
+    .catch(err => {
+        setErrors({
+            ...errors,
+            [e.target.name]: err.errors
+        })
+    })
+    }
     
     return (
         <form onSubmit={formSubmit}>

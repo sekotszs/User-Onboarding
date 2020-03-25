b0039f6d12ddb1cb381ca167d3c077ff664b663b
Zoe Stokes <sekotszs@gmail.com>
2020-03-25 14:02:49 -0700

    part 2

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,13 +2,29 @@ import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
+const formSchema = yup.object().shape({
+    name:yup.string().required("Name is required"),
+    email: yup.string().email().required("Email is required"),
+    password: yup.string().min(7,"Needs to be more that 7 characters").required('Where is the password?'),
+    terms: yup.boolean().oneOf([true], "Agree to the terms or else")
+})
+
 function Form() {
     const [formState, setFormState] = useState({
-        Name: '',
-        Email: '',
-        Password: '',
-        Terms: '',
+        name: '',
+        email: '',
+        password: '',
+        terms: '',
     })
+
+    const[errors, setErrors] = useState({
+        name:'',
+        email:'',
+        password:'',
+        terms:'',
+    });
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    
     return (
         <form onSubmit={formSubmit}>
             <label htmlFor="name">
@@ -20,6 +36,7 @@ function Form() {
                     value={formState.name}
                     onChange={inputChange}
                 />
+                {errors.name.length > 0 ? <p>{errors.name}</p> :null}
             </label>
             <label htmlFor='email'>
                 Email
@@ -30,6 +47,7 @@ function Form() {
                     value={formState.email}
                     onChange={inputChange}
                 />
+                {errors.email.length > 0 ? <p>{errors.email}</p> :null}
             </label>
             <label htmlFor='password'>
                 Password
@@ -40,6 +58,7 @@ function Form() {
                     value={formState.password}
                     onChange={inputChange}
                 />
+                {errors.password.length > 0 ? <p>{errors.password}</p> :null}
             </label>
             <label htmlFor='terms'>
                 Terms
@@ -50,6 +69,7 @@ function Form() {
                     value={formState.terms}
                     onChange={inputChange}
                 />
+                {errors.terms.length > 0 ? <p>{errors.terms}</p> :null}
 
             </label>
 
